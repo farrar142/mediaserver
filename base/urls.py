8561edd2217f105219e61fb7a336f2dcc09c1902
farrar142 <gksdjf1690@gmail.com>
2022-05-04 19:55:29 +0900

    media alias

--- base/urls.py
+++ base/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('mediaserver/', api.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
